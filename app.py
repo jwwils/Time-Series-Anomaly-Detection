96476f942806b6e1119fc6f8aba108609e14b1d4
jwwils <wilsonjoshuaw@gmail.com>
2023-08-18 15:09:38 +0200

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 import os
 import pandas as pd
 from sklearn.ensemble import IsolationForest
@@ -16,10 +16,6 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-def plot_original_data(data):
-    fig = px.line(data, x=data.index, y='Value', title='Original Data')
-    return plot(fig, output_type='div')
 
 @app.route('/')
 def index():
@@ -57,32 +53,37 @@ def process_file():
     data.columns = ['Value']
     data.fillna(method='ffill', inplace=True)
 
+    # Feature Engineering
+    # Rolling window features
+    data['rolling_mean'] = data['Value'].rolling(window=3).mean()
+    data['rolling_std'] = data['Value'].rolling(window=3).std()
+
+    # Remove NaN after feature engineering
+    data.dropna(inplace=True)
+
     # Detect anomalies using Isolation Forest
     model_if = IsolationForest(contamination=0.05)
-    data['anomaly_if'] = model_if.fit_predict(data[['Value']])
+    data['anomaly_if'] = model_if.fit_predict(data[['Value', 'rolling_mean', 'rolling_std']])
     anomalies_if = data[data['anomaly_if'] == -1]
-
-    # Plot using Plotly for Isolation Forest
-    fig_if = px.scatter(data, x=data.index, y='Value', color='anomaly_if', title='Anomaly Detection using Isolation Forest')
-    plot_if = plot(fig_if, output_type='div')
 
     # Detect anomalies using One-Class SVM
     model_svm = OneClassSVM(nu=0.05)
     data['anomaly_svm'] = model_svm.fit_predict(data[['Value']])
     anomalies_svm = data[data['anomaly_svm'] == -1]
 
-    # Plot using Plotly for One-Class SVM
-    fig_svm = px.scatter(data, x=data.index, y='Value', color='anomaly_svm', title='Anomaly Detection using One-Class SVM')
-    plot_svm = plot(fig_svm, output_type='div')
-
-    # Plot original data
-    original_plot = plot_original_data(data)
-
-    # Save the processed data with anomalies highlighted
+    # Exporting results
     export_filename = "processed_" + filename
     data.to_csv(os.path.join(app.config['UPLOAD_FOLDER'], export_filename))
 
-    return render_template('results.html', plot_if=plot_if, plot_svm=plot_svm, original_plot=original_plot, anomalies_if=anomalies_if, anomalies_svm=anomalies_svm, export_filename=export_filename)
+    original_plot = plot(px.line(data, x=data.index, y='Value', title='Original Data'), output_type='div')
+
+    return render_template('results.html', 
+                           original_plot=original_plot,
+                           plot_if=plot(px.scatter(data, x=data.index, y='Value', color='anomaly_if', title='Isolation Forest'), output_type='div'),
+                           plot_svm=plot(px.scatter(data, x=data.index, y='Value', color='anomaly_svm', title='One-Class SVM'), output_type='div'),
+                           anomalies_if=anomalies_if,
+                           anomalies_svm=anomalies_svm,
+                           export_filename=export_filename)
 
 @app.route('/download/<filename>', methods=['GET'])
 def download_file(filename):
